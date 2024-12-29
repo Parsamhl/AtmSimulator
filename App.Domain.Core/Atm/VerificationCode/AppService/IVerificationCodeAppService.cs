80b26bfbdc780d470ad86f799aea2b7a38468b81
parsa <mparsah$@gmail.com>
2024-12-29 15:20:21 +0330

    Added File Verification

---
+++ App.Domain.Core/Atm/VerificationCode/AppService/IVerificationCodeAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Atm.VerificationCode.AppService
+{
+    public interface IVerificationCodeAppService
+    {
+
+        string GenerateAndSaveCode();
+        void SaveVerificationCodeToFile(string verificationCode);
+        Result.Entitiy.Result ValidateCode(string enteredCode);
+    }
+}
